1b70f748d605e05cad666acbf24f61795049107d
Tonatiuh Núñez <tonatiuhnb@gmail.com>
2015-02-28 23:18:04 -0600

    added extension for snippets and for calculating pomodoros

---
+++ MySnippets.ftplugin/main.js
@@ -0,0 +1,15 @@
+define(function(require, exports, module) {
+    var Extensions = require('ft/core/extensions').Extensions;
+
+    Extensions.addSnippet({
+        ';pmd': '## Pomodoro.timer\
+\nFirst pomodoro for 25 minutes\
+\nBreak for 5 minutes\
+\nSecond pomodoro for 25 minutes\
+\nBreak for 5 minutes\
+\nThird pomodoro for 25 minutes\
+\nBreak for 5 minutes\
+\nFourth pomodoro for 25 minutes\
+\nLong break for 15 minutes'
+    });
+});
---
+++ PomodoroTotalTime.ftplugin/main.js
@@ -0,0 +1,34 @@
+define(function(require, exports, module) {
+    var Extensions = require('ft/core/extensions').Extensions;
+
+    Extensions.addInit(function (editor) {
+      editor.addKeyMap({
+            'Cmd-H' : function (editor) {
+                var tree         = editor.tree();
+                var pomodoros    = 0;
+                var totalMinutes = 0;
+
+                tree.nodes().forEach(function(node) {
+                  if (!editor.nodeIsHiddenInFold(node)) {
+                    tags = node.tags();
+
+                    if (tags.p != undefined) {
+                      pomodoros = pomodoros + tags.p.match(/x/g).length;
+                    }
+
+                    if (tags.pt != undefined) {
+                      var minutesArray = tags.pt.split(',');
+
+                      for(var i = 0, length = minutesArray.length; i < length; i++){
+                          totalMinutes += parseInt(minutesArray[i]);
+                      }
+                    }
+
+                  }
+                });
+
+                editor.replaceSelection('pomodoros: ' + pomodoros + '. minutes: ' + totalMinutes);
+            }
+        });
+    });
+});
