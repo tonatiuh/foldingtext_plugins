aaaa714027d615a8f8270d9d411c5ed0ea2da386
Tonatiuh Núñez <tonatiuhnb@gmail.com>
2015-07-27 05:09:58 -0500

    wip

--- PomodoroTotalTime.ftplugin/main.js
+++ PomodoroTotalTime.ftplugin/main.js
@@ -1,39 +1,59 @@
 define(function(require, exports, module) {
-    var Extensions = require('ft/core/extensions').Extensions;
+  var Extensions = require('ft/core/extensions').Extensions;
 
-    Extensions.addInit(function (editor) {
-      editor.addKeyMap({
-            'Cmd-H' : function (editor) {
-                var tree         = editor.tree();
-                var pomodoros    = 0;
-                var totalMinutes = 0;
-                var totalHours   = 0;
+  Extensions.addInit(function (editor) {
+    editor.addKeyMap({
+      'Cmd-H' : function (editor) {
+        var tree         = editor.tree();
+        var pomodoros    = 0;
+        var totalMinutes = 0;
+        var totalHours   = 0;
 
-                tree.nodes().forEach(function(node) {
-                  if (!editor.nodeIsHiddenInFold(node)) {
-                    tags = node.tags();
+        tree.nodes().forEach(function(node) {
+          if (!editor.nodeIsHiddenInFold(node)) {
+            tags = node.tags();
 
-                    if (tags.p != undefined) {
-                      pomodoros = pomodoros + tags.p.match(/x/g).length;
-                    }
-
-                    if (tags.pt != undefined) {
-                      var minutesArray = tags.pt.split(',');
-
-                      for(var i = 0, length = minutesArray.length; i < length; i++){
-                          totalMinutes += parseInt(minutesArray[i]);
-                      }
-                    }
-
-                  }
-                });
-
-                if (totalMinutes > 0) {
-                  totalHours = totalMinutes / 60;
-                }
-
-                editor.replaceSelection('pomodoros: ' + pomodoros + '. hours: ' + totalHours);
+            if (tags.p != undefined) {
+              pomodoros = pomodoros + tags.p.match(/x/g).length;
             }
+
+            if (tags.pt != undefined) {
+              var minutesArray = tags.pt.split(',');
+
+              for(var i = 0, length = minutesArray.length; i < length; i++){
+                totalMinutes += parseInt(minutesArray[i]);
+              }
+            }
+
+          }
         });
+
+        if (totalMinutes > 0) {
+          totalHours = totalMinutes / 60;
+        }
+
+        editor.replaceSelection('pomodoros: ' + pomodoros + '. hours: ' + totalHours);
+      },
+
+      'Cmd-K' : function (editor) {
+        var pomodoros    = 0;
+        var totalMinutes = 0;
+
+        var nodeHasTag = function(node, tag) {
+          return (eval('node.tags().' + tag) != undefined)
+        };
+
+        tree.nodes().forEach(function(node) {
+          if (nodeHasTag(node, 'p') == true) {
+            pomodoros = tags.p.match(/x/g).length;
+          }
+          //
+          // if nodeHasTag(node, 'pt') {
+          //   var minutesArray = tags.pt.split(',');
+          //   totalMinutes = eval(minutesArray.join('+'))
+          // }
+        });
+      }
     });
+  });
 });
